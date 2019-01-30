3ed01c80866d6343732306e4e6fd68523bb38593
Lucas Huang <hello@superko.org>
2019-01-30 14:48:31 -0500

    fix import. JSON write-to-file WIP, commented out

--- test/index.js
+++ test/index.js
@@ -1,8 +1,13 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
-const index_1 = require("../src/index");
+// import * as fs from "fs";
+const index_1 = require("../dist/index");
 const { figmaToken } = require("./secrets");
 const fileId = "7pVyYhhKgQdkdj7PRQnVrm";
 index_1.getFigmaRaw(figmaToken, fileId).then(val => {
     console.log(val);
+    // fs.writeFile("./test/results.json", val, err => {
+    //   if (err) return console.log(err);
+    //   console.log("File saved successfully.");
+    // });
 });
--- test/index.ts
+++ test/index.ts
@@ -1,8 +1,13 @@
-import { getFigmaRaw } from "../src/index";
+// import * as fs from "fs";
+import { getFigmaRaw } from "../dist/index";
 
 const { figmaToken } = require("./secrets");
 const fileId = "7pVyYhhKgQdkdj7PRQnVrm";
 
 getFigmaRaw(figmaToken, fileId).then(val => {
   console.log(val);
+  // fs.writeFile("./test/results.json", val, err => {
+  //   if (err) return console.log(err);
+  //   console.log("File saved successfully.");
+  // });
 });
